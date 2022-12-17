f5087de047c3a920e8d8132694c58da49703faa1
EugenefedorovPro <eugene.proskulikov@gmail.com>
2022-12-17 22:28:23 +0200

    setup_depth_rhyme - lower depth made

--- rhyme/utils/setup_depth_rhyme.py
+++ rhyme/utils/setup_depth_rhyme.py
@@ -2,24 +2,24 @@
 
 
 def setup_depth_rhyme(depth_time_for_rhyming):
-    if depth_time_for_rhyming == 2:
-        max_length_pat_of_ipa = 6
-        list_score_numbers = range(0, 45, 5)
-        max_number_hard_sounds_in_one_pat = 1
     if depth_time_for_rhyming == 1:
         max_length_pat_of_ipa = 5
         list_score_numbers = range(0, 30, 5)
         max_number_hard_sounds_in_one_pat = 1
+    if depth_time_for_rhyming == 2:
+        max_length_pat_of_ipa = 6
+        list_score_numbers = range(0, 45, 5)
+        max_number_hard_sounds_in_one_pat = 1
     if depth_time_for_rhyming == 3:
         max_length_pat_of_ipa = 6
-        list_score_numbers = range(0, 50, 5)
+        list_score_numbers = range(0, 60, 5)
         max_number_hard_sounds_in_one_pat = 1
     if depth_time_for_rhyming == 4:
-        max_length_pat_of_ipa = 7
-        list_score_numbers = range(0, 100, 5)
+        max_length_pat_of_ipa = 6
+        list_score_numbers = range(0, 75, 5)
         max_number_hard_sounds_in_one_pat = 1
     if depth_time_for_rhyming == 5:
         max_length_pat_of_ipa = 7
         list_score_numbers = range(0, 100, 5)
-        max_number_hard_sounds_in_one_pat = 2
+        max_number_hard_sounds_in_one_pat = 1
     return max_length_pat_of_ipa, list_score_numbers, max_number_hard_sounds_in_one_pat
