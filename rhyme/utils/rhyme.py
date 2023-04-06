5ffbfae389f9552f8330783bdb0fd83f02365a58
EugenefedorovPro <eugene.proskulikov@gmail.com>
2023-04-06 21:12:28 +0300

    rhyme words not in db

--- rhyme/utils/rhyme.py
+++
@@ -1,5 +0,0 @@
-from rhyme_rus.rhyme import rhyme, rhyme_with_stresses
-
-def rhyme(unstressed_word):
-    table, all_stresses, stressed_word = rhyme_with_stresses(unstressed_word)
-    return table, all_stresses, stressed_word
--- rhyme/views/views_rhyme_results.py
+++ rhyme/views/views_rhyme_results.py
@@ -1,13 +1,17 @@
-from pathlib import Path
+from rhyme_rus.rhyme import rhyme_with_stresses
 from django.shortcuts import render
-from rhyme.utils.rhyme import rhyme
-print("))))))))))))))))))))))))", Path(__name__))
+
+
 
 def rhyme_results(request):
-   print("__________________", request.POST)
    unstressed_word = request.POST["rhyme_word"]
-   print("________________", request.POST["rhyme_word"])
-   table_of_rhymes, all_stresses, stressed_word = rhyme(unstressed_word)
-   table_of_rhymes = table_of_rhymes.to_html()
-   context = {"table_of_rhymes": table_of_rhymes, "stressed_word": stressed_word}
-   return render(request, "rhyme_results.html", context)
+   try:
+      table_of_rhymes, all_stresses, stressed_word = rhyme_with_stresses(unstressed_word)
+      table_of_rhymes.to_string(index = False, col_space = -15)
+      table_of_rhymes = table_of_rhymes.to_html()
+      context = {"table_of_rhymes": table_of_rhymes, "stressed_word": stressed_word}
+      return render(request, "rhyme_results.html", context)
+   except:
+      all_stresses = rhyme_with_stresses(unstressed_word)[1]
+      context = {'unstressed_word': unstressed_word, 'all_stresses': all_stresses}
+      return render(request, 'rhyme.html', context)
