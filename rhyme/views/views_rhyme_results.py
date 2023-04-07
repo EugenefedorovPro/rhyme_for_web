5edc146316402240140a2a2188ab668ceaf5ec61
EugenefedorovPro <eugene.proskulikov@gmail.com>
2023-04-07 10:52:20 +0300

    words with no stress in db

--- rhyme/views/views_rhyme_results.py
+++ rhyme/views/views_rhyme_results.py
@@ -4,12 +4,13 @@ from django.shortcuts import render
 
 
 def rhyme_results(request):
-   unstressed_word = request.POST["rhyme_word"]
+   unstressed_word = request.POST["target_word"]
    try:
       table_of_rhymes, all_stresses, stressed_word = rhyme_with_stresses(unstressed_word)
       table_of_rhymes.to_string(index = False, col_space = -15)
+      number_rhymes = len(table_of_rhymes)
       table_of_rhymes = table_of_rhymes.to_html()
-      context = {"table_of_rhymes": table_of_rhymes, "stressed_word": stressed_word}
+      context = {"table_of_rhymes": table_of_rhymes, "stressed_word": stressed_word, "number_rhymes": number_rhymes}
       return render(request, "rhyme_results.html", context)
    except:
       all_stresses = rhyme_with_stresses(unstressed_word)[1]
