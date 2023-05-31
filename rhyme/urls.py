631f944d9abbf3fa745a7d165fe8d0ee66a34ef2
EugenefedorovPro <eugene.proskulikov@gmail.com>
2023-05-31 12:07:15 +0300

    add base_rhyme.html

--- rhyme/urls.py
+++ rhyme/urls.py
@@ -6,6 +6,7 @@ app_name = 'rhyme'
 urlpatterns = [
     path('', views_title.title, name = 'title'),
     path('rhyme/', views_rhyme.rhyme, name = 'rhyme'),
+    path('rhyme', views_rhyme.rhyme, name = 'rhyme'),
     path('rhyme/rhyme_results/', views_rhyme_results.rhyme_results, name='rhyme_results')
 
     ]
