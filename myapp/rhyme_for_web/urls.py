3b8f0efcc7bbac0a42267e6be3c4f943962b6bb6
EugenefedorovPro <eugene.proskulikov@gmail.com>
2023-11-27 17:17:37 +0200

    no /rhyme after domain name

--- myapp/rhyme_for_web/urls.py
+++ myapp/rhyme_for_web/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('rhyme/', include('rhyme.urls')),
     path('admin/', admin.site.urls),
+    path('', include('rhyme.urls')),
 ]
