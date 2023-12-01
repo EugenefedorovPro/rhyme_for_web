d949fb1cca99bafcd059a37c49e89e18039f5f4c
EugenefedorovPro <eugene.proskulikov@gmail.com>
2023-12-01 21:18:30 +0200

    all work, gunicorn optimized for pdb debug

--- myapp/entrypoint.sh
+++ myapp/entrypoint.sh
@@ -3,4 +3,8 @@
 python manage.py migrate
 python manage.py collectstatic --noinput
 
-gunicorn rhyme_for_web.wsgi:application --bind 0.0.0.0:8000
+# version with long timeout for pdb debug
+gunicorn rhyme_for_web.wsgi:application --bind 0.0.0.0:8000 --workers 16 --timeout 300
+
+# for deploy
+# gunicorn rhyme_for_web.wsgi:application --bind 0.0.0.0:8000
--- myapp/rhyme/views/views_title.py
+++ myapp/rhyme/views/views_title.py
@@ -1,15 +1,5 @@
 from django.shortcuts import render
-<<<<<<< HEAD
 
 
 def title(request):
     return render(request, 'title.html')
-=======
-from django.http import HttpResponse
-from django.shortcuts import render, redirect
-
-def title(request):
-    return render(request, 'title.html')
-
-# Create your views here.
->>>>>>> 3553f703e382e2fab61378fa69d1b3fcc4114bed
