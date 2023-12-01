33645f9d5b306c9f81bf4070bdbb5b7422c5a412
EugenefedorovPro <eugene.proskulikov@gmail.com>
2023-12-01 18:22:18 +0200

    merge

--- myapp/rhyme/urls.py
+++ myapp/rhyme/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+<<<<<<< HEAD
 from .views import (
     views_title,
     views_rhyme,
@@ -21,3 +22,18 @@ urlpatterns = [
     ),
     path("favicon.ico", RedirectView.as_view(url="/static/favicon.ico")),
 ]
+=======
+from .views import views_title, views_rhyme, views_rhyme_results, views_stress, views_transcribe
+
+app_name = 'rhyme'
+
+urlpatterns = [
+    path('', views_title.title, name = 'title'),
+    path('rhyme', views_rhyme.rhyme, name = 'rhyme'),
+    path('stress', views_stress.stress, name = 'stress'),
+    path('transcribe', views_transcribe.transcribe, name = 'transcribe'),
+    # path('rhyme/', views_rhyme.rhyme, name = 'rhyme'),
+    path('rhyme/rhyme_results/', views_rhyme_results.rhyme_results, name='rhyme_results')
+    ]
+
+>>>>>>> 3553f703e382e2fab61378fa69d1b3fcc4114bed
