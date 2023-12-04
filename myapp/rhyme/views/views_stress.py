96e5c22d6da3991ad9813d69643f1a79f7077b62
EugenefedorovPro <eugene.proskulikov@gmail.com>
2023-12-04 12:40:32 +0200

    stress.html desing

--- myapp/rhyme/views/views_stress.py
+++ myapp/rhyme/views/views_stress.py
@@ -1,4 +1,13 @@
+import pdb
+# from put_stress_rus.put_stress import put_stress
 from django.shortcuts import render
 
 def stress(request):
+    # word_unstressed = request.GET.get('word_unstressed')
+    # word_stressed = put_stress(word_unstressed)
+    # pdb.set_trace()
+    # context = {
+    #         'word_stressed': word_stressed,
+    #         }
     return render(request, 'stress.html')
+
