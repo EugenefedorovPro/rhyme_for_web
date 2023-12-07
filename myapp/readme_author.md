1b4e09ac6221cba963ce1caa41845d6e8bf5e9f5
EugenefedorovPro <eugene.proskulikov@gmail.com>
2023-12-07 17:23:47 +0200

    readme: author, rhyme, stress, transcribe

---
+++ myapp/readme_author.md
@@ -0,0 +1,9 @@
+### Первый открытый алгоритм для подбора рифм
+
+- В основе: [en.wiktionary.org](https://en.wiktionary.org/wiki/Wiktionary:Main_Page), две авторские нейросети:
+	- для постановки ударения.
+	- для транскрипции русских слов в международный фонетический алфавит.
+- Технологический стек: Python, Keras, Django, JS, html, css, docker, git, vim.
+- python package на [GitHub](https://github.com/EugenefedorovPro/rhyme_rus), а также документация на английском.
+
+Автор теории рифм, алгоритма и разработчик проекта: *Евгений Проскуликов*
--- myapp/rhyme/views/views_about.py
+++ myapp/rhyme/views/views_about.py
@@ -1,6 +1,39 @@
+import markdown2
 from django.shortcuts import render
+from pathlib import Path
+
 
 def get_about(request):
-    title = "Первый открытый алгоритм для подбора рифм"
-    context = {"title": title}
+    # import about rhyme md
+    path = Path("__file__").parent.parent
+    path_rhyme = path / "readme_rhyme.md"
+    with open(path_rhyme, "r", encoding="utf-8") as file:
+        about_md = file.read()
+    rhyme_html = markdown2.markdown(about_md)
+
+    # import about author md
+    path_author = path / "readme_author.md"
+    with open(path_author, "r", encoding="utf-8") as file:
+        author_md = file.read()
+        author_html = markdown2.markdown(author_md)
+
+    # import about stress md
+    path_stress = path / "readme_stress.md"
+    with open(path_stress, "r", encoding="utf-8") as file:
+        stress_md = file.read()
+        stress_html = markdown2.markdown(stress_md)
+
+    # import about stress md
+    path_transcribe = path / "readme_transcribe.md"
+    with open(path_transcribe, "r", encoding="utf-8") as file:
+        transcribe_md = file.read()
+        transcribe_html = markdown2.markdown(transcribe_md)
+
+    context = {
+        "rhyme_html": rhyme_html,
+        "author_html": author_html,
+        "stress_html": stress_html,
+        "transcribe_html": transcribe_html,
+    }
+
     return render(request, "about.html", context)
